9cf8f36511106adebdbe1da1b9dbe7422e13d509
Danvercraft <san290200@hotmail.com>
2022-01-12 00:22:47 -0500

    Actualización 1.4.

--- app/Http/Controllers/CotizacionController.php
+++ app/Http/Controllers/CotizacionController.php
@@ -8,79 +8,45 @@ use App\Http\Requests\UpdateCotizacionRequest;
 
 class CotizacionController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
-        //
+        
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function create()
     {
-        //
+        
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreCotizacionRequest  $request
-     * @return \Illuminate\Http\Response
-     */
+
     public function store(StoreCotizacionRequest $request)
     {
-        //
+        
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Cotizacion  $cotizacion
-     * @return \Illuminate\Http\Response
-     */
+
     public function show(Cotizacion $cotizacion)
     {
-        //
+        
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Cotizacion  $cotizacion
-     * @return \Illuminate\Http\Response
-     */
+
     public function edit(Cotizacion $cotizacion)
     {
-        //
+        
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateCotizacionRequest  $request
-     * @param  \App\Models\Cotizacion  $cotizacion
-     * @return \Illuminate\Http\Response
-     */
+
     public function update(UpdateCotizacionRequest $request, Cotizacion $cotizacion)
     {
-        //
+        
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Cotizacion  $cotizacion
-     * @return \Illuminate\Http\Response
-     */
+
     public function destroy(Cotizacion $cotizacion)
     {
-        //
+        
     }
 }
--- app/Http/Controllers/CotizaciondetalleController.php
+++ app/Http/Controllers/CotizaciondetalleController.php
@@ -8,79 +8,44 @@ use App\Http\Requests\UpdateCotizaciondetalleRequest;
 
 class CotizaciondetalleController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
-        //
+        
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function create()
     {
-        //
+        
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreCotizaciondetalleRequest  $request
-     * @return \Illuminate\Http\Response
-     */
+
     public function store(StoreCotizaciondetalleRequest $request)
     {
-        //
+        
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Cotizaciondetalle  $cotizaciondetalle
-     * @return \Illuminate\Http\Response
-     */
+
     public function show(Cotizaciondetalle $cotizaciondetalle)
     {
-        //
+        
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Cotizaciondetalle  $cotizaciondetalle
-     * @return \Illuminate\Http\Response
-     */
+
     public function edit(Cotizaciondetalle $cotizaciondetalle)
     {
-        //
+        
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateCotizaciondetalleRequest  $request
-     * @param  \App\Models\Cotizaciondetalle  $cotizaciondetalle
-     * @return \Illuminate\Http\Response
-     */
+
     public function update(UpdateCotizaciondetalleRequest $request, Cotizaciondetalle $cotizaciondetalle)
     {
-        //
+        
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Cotizaciondetalle  $cotizaciondetalle
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Cotizaciondetalle $cotizaciondetalle)
     {
-        //
+        
     }
 }
