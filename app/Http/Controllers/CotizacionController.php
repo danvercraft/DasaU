fb150b088d3351425cfe9d63796125c565a53157
Danvercraft <san290200@hotmail.com>
2022-01-12 08:53:03 -0500

    Actualización 1.5.

--- app/Http/Controllers/CotizacionController.php
+++ app/Http/Controllers/CotizacionController.php
@@ -5,48 +5,64 @@ namespace App\Http\Controllers;
 use App\Models\Cotizacion;
 use App\Http\Requests\StoreCotizacionRequest;
 use App\Http\Requests\UpdateCotizacionRequest;
+use App\Models\Provider;
 
 class CotizacionController extends Controller
 {
 
     public function index()
     {
-        
+        $cotizacions = Cotizacion::get();
+        return view('admin.cotizacion.index', compact('cotizacions'));
     }
 
 
     public function create()
     {
-        
+        $providers = Provider::get();
+        return view('admin.cotizacion.index', compact('providers'));
+
     }
 
 
     public function store(StoreCotizacionRequest $request)
     {
-        
+        Cotizacion::create($request->all());
+
+        if($request->hasFile('picture')){
+            $file = $request->file('picture');
+            $image_name = time().'_'.$file->getClientOriginalName();
+            $file->move(public_path("/image"),$image_name);
+        }
+
+        return redirect()->route('cotizacions.index');
     }
 
 
     public function show(Cotizacion $cotizacion)
     {
-        
+        return view('admin.cotizacion.show', compact('cotizacion'));
+
     }
 
 
     public function edit(Cotizacion $cotizacion)
     {
-        
+        return view('admin.cotizacion.edit', compact('cotizacion'));
+
     }
 
 
     public function update(UpdateCotizacionRequest $request, Cotizacion $cotizacion)
     {
-        
+        $cotizacion->update($request->all());
+        return redirect()->route('cotizacions.index');
     }
 
 
     public function destroy(Cotizacion $cotizacion)
     {
-        
+        $cotizacion->delete();
+        return redirect()->route('cotizacions.index');
     }
 }
--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -11,42 +11,46 @@ class TagController extends Controller
 
     public function index()
     {
-        
+        $tags = Tag::get();
+        return view('admin.tag.index', compact('tags'));
     }
 
 
     public function create()
     {
-        
+        return view('admin.tag.index', compact('tags'));
     }
 
 
     public function store(StoreTagRequest $request)
     {
-        
+        Tag::create($request->all());
+        return redirect()->route('tags.index');
     }
 
 
     public function show(Tag $tag)
     {
-        
+        return view('admin.tag.show', compact('tag'));
     }
 
 
     public function edit(Tag $tag)
     {
-        
+        return view('admin.tag.edit', compact('tag'));
     }
 
 
     public function update(UpdateTagRequest $request, Tag $tag)
     {
-        
+        $tag->update($request->all());
+        return redirect()->route('tags.index');
     }
 
 
     public function destroy(Tag $tag)
     {
-        
+        $tag->delete();
+        return redirect()->route('tags.index');
     }
 }
